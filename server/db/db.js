85113b7017efc832e905f7f2472876ce4f9a2777
gdomorski <gregdomorski@gmail.com>
2016-04-18 21:52:29 -0400

    fix sequalize

--- server/db/db.js
+++ server/db/db.js
@@ -1,14 +1,21 @@
 var Sequelize = require("sequelize");
-
-var db = new Sequelize('twitter_users', '', '', {
-  host: 'localhost',
-  dialect: 'postgres',
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-})
+var db;
+if (process.env.PORT) {
+  db = new Sequelize("d9veam8sf1mste", "sxngmhdhidhcym", "sItWtmfNTR1ff6oF6Q6i1i0W3B", {
+    host: 'ec2-23-21-215-184.compute-1.amazonaws.com',
+    dialect: 'postgres'
+  });
+}else{
+  db = new Sequelize('twitter_users', '', '', {
+    host: 'localhost',
+    dialect: 'postgres',
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    }
+  })
+}
 
 var User = db.define('user', {
   fname: Sequelize.STRING,
