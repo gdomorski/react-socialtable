dafc4cbee5fcbea81095b7311ff8c82dd8568e15
gdomorski <gregdomorski@gmail.com>
2016-04-18 23:12:16 -0400

    try again

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ var express = require('express');
 
 
 //POSTGRESQL
-var conString = process.env.DATABASE_URL || {host: "localhost", port: 5432, ssl: true}
+var conString = process.env.DATABASE_URL || {host: "localhost", port: 5432, user: "", password: "", database: "twitter_users"}
 // 'postgres://localhost:5432/'
 console.log(conString, 'CONSTRINGGGGGGG')
 var client = new pg.Client(conString);
